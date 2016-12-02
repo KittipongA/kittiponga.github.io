34e4eb236c69a379b0b4857ad2cd31ea0b44dd72
KittipongA <kittipong@readyplanet.com>
2016-12-02 15:37:14 +0700

    edit sw.js

--- sw.js
+++ sw.js
@@ -2,6 +2,20 @@ if ('serviceWorker' in navigator) {
     navigator.serviceWorker.register('/sw.js').then(function(registration) {
         // Registration was successful
         console.log('ServiceWorker registration successful with scope: ', registration.scope);
+        var CACHE_NAME = 'my-site-cache-v1';
+        var urlsToCache = [
+            '/index.html',
+        ];
+
+        self.addEventListener('install', function(event) {
+            // Perform install steps
+            event.waitUntil(
+                caches.open(CACHE_NAME).then(function(cache) {
+                    console.log('Opened cache');
+                    return cache.addAll(urlsToCache);
+                })
+            );
+        });
     }).catch(function(err) {
         // registration failed :(
         console.log('ServiceWorker registration failed: ', err);
